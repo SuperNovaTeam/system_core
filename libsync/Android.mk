f4cd9ac1db0c64251d95c53615481bbfba294cb0
Samuele Ciprietti <samuele.ciprietti@gmail.com>
2013-10-18 23:33:50 +0200

    sync_test: Use -fno-strict-aliasing

--- libsync/Android.mk
+++ libsync/Android.mk
@@ -5,6 +5,7 @@ LOCAL_SRC_FILES := sync.c
 LOCAL_MODULE := libsync
 LOCAL_MODULE_TAGS := optional
 LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_CFLAGS += -fno-strict-aliasing 
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
